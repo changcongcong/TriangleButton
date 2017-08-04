bd324a7bd4cccca0cf1b6288e1f6d71e0554f9da
congcong.chang <congcong.chang@lucis-tech.com>
2017-08-04 16:45:35 +0800

    Initial Commit

---
+++ TriangleButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TriangleButton
+//
+//  Created by admin on 2017/8/4.
+//  Copyright © 2017年 常丛丛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TriangleButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TriangleButton
+//
+//  Created by admin on 2017/8/4.
+//  Copyright © 2017年 常丛丛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
